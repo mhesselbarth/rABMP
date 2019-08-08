6ff13d9ae42a4a54cb28ac5e0d7c95db82332676
mhesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2019-08-08 15:13:29 +0200

    Cleaning input data/scripts/examples

--- R/data.R
+++ R/data.R
@@ -1,8 +1,8 @@
 #' Example data set
 #'
-#' The data set contains an exemplary tree population within a 500x500 observation window.
-#' Each indiviual tree is described by its x- and y-coordinate,
-#' tree species, type diameter at breast height (DBH)
+#' The data set contains an exemplary tree population within a 500 x 500 observation
+#' window. Each indiviual tree is described by its x- and y-coordinate, tree species,
+#' type, and diameter at breast height (DBH)
 #'
 #' @format A tibble with 1,340 rows and 5 variables:
 #'
@@ -16,20 +16,3 @@
 #'
 #' bhd: diameter at breast height
 "example_input_data"
-
-#' Input data
-#'
-#' Input data  year 1999
-#'
-#' @format A dataframe with 14373 rows and 5 variables:
-#'
-#' x: x coordinate
-#'
-#' y: y coordinate
-#'
-#' Species: species
-#'
-#' DBH: type (e.g. "Adult")
-#'
-#' Type: diameter at breast height
-"input_data"
